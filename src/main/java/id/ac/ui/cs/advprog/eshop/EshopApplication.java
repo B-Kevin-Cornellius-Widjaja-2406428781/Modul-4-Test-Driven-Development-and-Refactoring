bce15d391cd2390c0d41567ddcdea55414727924
Kevin Cornellius Widjaja <kevinbananax@gmail.com>
2026-02-19 18:21:27 +0700

    hotfix: change EshopApplication class from final to non-final

--- src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/EshopApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public final class EshopApplication {
+public class EshopApplication {
     private EshopApplication() {
     }
 
