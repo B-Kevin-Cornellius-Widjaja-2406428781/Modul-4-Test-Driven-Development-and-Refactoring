5cbf054d418e4f8c29759a23b99dd51f10b5f1d9
Kevin Cornellius Widjaja <kevinbananax@gmail.com>
2026-03-03 12:53:14 +0700

    refactor: simplify update method in CarRepository by using enhanced for loop

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -36,8 +36,7 @@ public class CarRepository implements ICarRepository {
 
     @Override
     public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
+        for (Car car : carData) {
             if (car.getCarId().equals(id)) {
                 car.setCarName(updatedCar.getCarName());
                 car.setCarColor(updatedCar.getCarColor());
